0b3eb102187c2bc3aa9028249b7931b0741f0e3f
ccheney <snarfoogle@gmail.com>
2018-05-22 18:46:35 -0700

    malloc fix

--- ft_strnew.c
+++ ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: ccheney <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/21 15:46:57 by ccheney           #+#    #+#             */
-/*   Updated: 2018/05/21 15:47:01 by ccheney          ###   ########.fr       */
+/*   Updated: 2018/05/22 18:46:08 by ccheney          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_strnew(size_t size)
 {
 	char *temp;
 
-	temp = (char *)(ft_memalloc(sizeof(char) * size + 1));
+	temp = (char *)(malloc(sizeof(char) * size + 1));
 	if (!temp)
 		return (NULL);
 	ft_memset(temp, (int)'\0', size + 1);
