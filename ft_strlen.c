fc34abf584919936cc9bd16bc5d949d7184389cc
ccheney <snarfoogle@gmail.com>
2018-05-23 11:28:47 -0700

    ?

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: ccheney <ccheney@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/26 15:13:14 by ccheney           #+#    #+#             */
-/*   Updated: 2018/02/28 16:16:41 by ccheney          ###   ########.fr       */
+/*   Updated: 2018/05/21 18:24:43 by ccheney          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ size_t	ft_strlen(const char *str)
 	unsigned int len;
 
 	len = 0;
-	while (str[len])
+	while (str[len] != '\0')
 		len++;
 	return (len);
 }
